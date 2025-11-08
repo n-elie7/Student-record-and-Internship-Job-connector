45a07de3e242ff08ec415a162fe9c01284626741
Niyubwayo Irakoze Elie <n.elie@alustudent.com>
2025-11-08 23:24:15 +0200

    Implemented function that create connection object

--- database.py
+++ database.py
@@ -1,2 +1,11 @@
-# I'm going to implement database functions that will initialize
-# Our SQL connection
+import sqlite3
+
+DEFAULT_DB = "prod.db"
+
+def get_connection(db_path=DEFAULT_DB):
+    """
+    This function will create connection object from sqlite3
+    """
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
