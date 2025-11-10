d87d42977ae97c80cb84aa7828e7a4d6897dfd28
brina-cloud <s.kaliza1@alustudent.com>
2025-11-10 15:13:47 +0200

    changed the roll_no attribute to reg_no

--- students.py
+++ students.py
@@ -1,15 +1,15 @@
 import sqlite3
 from database import DEFAULT_DB, get_connection
 
-def add_student(name, roll_no, age=None, course=None, gpa=None, db_path=DEFAULT_DB):
+def add_student(name, reg_no, age=None, course=None, gpa=None, db_path=DEFAULT_DB):
 
     conn = get_connection(db_path)
     #Get connection to the SQL database
     cur = conn.cursor()
     try:
         cur.execute(
-            "INSERT INTO students (name, roll_no, age, course, gpa) VALUES (?, ?, ?, ?, ?)",
-            (name.strip(), roll_no.strip(), age.strip(), course.strip(), gpa.strip())
+            "INSERT INTO students (name, reg_no, age, course, gpa) VALUES (?, ?, ?, ?, ?)",
+            (name.strip(), reg_no.strip(), age.strip(), course.strip(), gpa.strip())
         )
         conn.commit()
         return cur.lastrowid
@@ -18,10 +18,13 @@ def add_student(name, roll_no, age=None, course=None, gpa=None, db_path=DEFAULT_
     finally:
         conn.close()
 
-def delete_student(roll_no, dp_path=DEFAULT_DB):
+def delete_student(reg_no, dp_path=DEFAULT_DB):
     #Get the function to connect with the SQL database
     conn = get_connection(dp_path)
     cur = conn.cursor()
     #We can delete student using the roll_no 
     try:
-        cur.execute("DELETE FROM students WHERE roll_no="?"", (roll_no.strip(), ))
+        cur.execute("DELETE FROM students WHERE roll_no=?", (reg_no.strip(), ))
+        conn.commit()
+        #The changes to be saved in database
+        
