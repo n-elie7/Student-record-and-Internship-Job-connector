f69f18307414b28be45bd78e7b3e9778fca1e7ab
Suwafa <s.iradukund2@alustudent.com>
2025-11-10 23:46:08 -0800

    connecting the sql command

--- internships.py
+++ internships.py
@@ -3,3 +3,4 @@ import sqlite3
 
 def add_internship(title, company, location, duration, stipend, description, deadline):
     conn = sqlite3.connect("student_internship.db")
+cur = sqlite3.connect.cursor()
