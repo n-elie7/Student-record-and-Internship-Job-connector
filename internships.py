9ae2ab41b6da8a86ec393d13c0fb8fb121928a0a
Suwafa <s.iradukund2@alustudent.com>
2025-11-11 12:02:23 -0800

    defining  a function named add_internship that’s meant to insert a new internship record into your database

--- internships.py
+++ internships.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+from database import DEFAULT_DB, get_connection
 
-def add_internship(title, company, location, duration, stipend, description, deadline):
-    conn = sqlite3.connect("student_internship.db")
+
+def add_internship(title, company, location, duration, stipend, description, deadline, db_path=DEFAULT_DB):
+    conn = get_connection(db_path)
 cur = sqlite3.connect.cursor()
