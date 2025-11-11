9295f729b077b73d2ff7eb510bc836d7494d7e6d
Suwafa <s.iradukund2@alustudent.com>
2025-11-11 12:07:20 -0800

    creating cursor to use in sql

--- internships.py
+++ internships.py
@@ -5,4 +5,4 @@ from database import DEFAULT_DB, get_connection
 
 def add_internship(title, company, location, duration, stipend, description, deadline, db_path=DEFAULT_DB):
     conn = get_connection(db_path)
-cur = sqlite3.connect.cursor()
+    cur = conn.cursor() # creates cursor
